19132ec3ded5a8f78199fe3f99f21972168fc585
sreenandhanpp <sreedeviv27801@gmail.com>
2024-01-10 20:55:58 +0530

    url-updated

--- config/connection.js
+++ config/connection.js
@@ -1,77 +1,74 @@
 // config/connection.js
+const dotenv = require("dotenv");
 
-const mongoose = require('mongoose');
+dotenv.config();
+const mongoose = require("mongoose");
 
-const uri = 'mongodb+srv://sreenandhanpp:sreenu9061@cluster0.ultw9ny.mongodb.net/studyhub?retryWrites=true&w=majority'; 
-process.env.MONGODB_UR
-mongoose.connect(uri, {
-  
-});
+const uri = process.env.MONGODB_URL;
+
+mongoose.connect(uri, {});
 
 const db = mongoose.connection;
 
-db.on('error', (err) => {
-  console.error('MongoDB connection error:', err);
+db.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
 });
 
-db.once('open', () => {
-  console.log('Connected to MongoDB');
+db.once("open", () => {
+  console.log("Connected to MongoDB");
 });
 
-const LoginSchema=new mongoose.Schema({
+const LoginSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  pw: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
 
-    firstname:{
-        type:String,
-        required:true
-    },  
-    lastname:{
-        type:String,
-        required:true
-    },  
-    pw:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },  
-    
-    phone:{
-        type:String,
-        required:true
-    },
-    photo:{
-        type:String,
-        required:false
-    }
-})
+  phone: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: false,
+  },
+});
 
+const AdminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  file: {
+    type: String,
+    required: true,
+  },
+  sem: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
+});
 
-const AdminSchema=new mongoose.Schema({
-
-    name:{
-        type:String,
-        required:true
-    },  
-    file:{
-        type:String,
-        required:true
-    },
-    sem:{
-        type: String,
-        required:true
-    },
-    content:{
-        type: String,
-        required:true
-    },
-    department:{
-        type: String,
-        required: true
-    }
-})
-
-const collection=new mongoose.model('collection1',LoginSchema)
-const AdminCollection=new mongoose.model('AdminData',AdminSchema)
-module.exports = { mongoose, db ,collection ,AdminCollection};
+const collection = new mongoose.model("collection1", LoginSchema);
+const AdminCollection = new mongoose.model("AdminData", AdminSchema);
+module.exports = { mongoose, db, collection, AdminCollection };
